1881d35e0460a34d281513c32c04c448ac4e6bb1
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2020-12-30 20:45:20 +0100

    Example of PBT with a linear function

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,16 @@
-import { systemUnderTest } from ".";
+import * as jsc from "jsverify";
+import { linear } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+it("the slope between two points should be constant", () => {
+  const constantSlope = jsc.forall("integer", "integer", (x1: number, x2: number) => {
+    if (x1 === x2) {
+      return true;
+    }
+    const y1 = linear(x1);
+    const y2 = linear(x2);
+    const slope = (y2 - y1) / (x2 - x1);
+    console.log({ x1, x2, slope });
+    return slope === 0.5;
   });
+  jsc.assert(constantSlope);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export function systemUnderTest(): boolean {
-  return true;
-}
+export const linear = (x: number): number => {
+  return 1 + 0.5 * x;
+};
